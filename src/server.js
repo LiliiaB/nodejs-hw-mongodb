1dc23a2d4394dfd980df4879d170e42b09fe4629
Lilia Levchenko <liliya.benatti@gmail.com>
2024-06-02 15:32:46 +0200

    edited path

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 import { env } from './utils/env.js';
 
-import { getAllContacts, getContactsById } from './services/students.js';
+import { getAllContacts, getContactsById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
