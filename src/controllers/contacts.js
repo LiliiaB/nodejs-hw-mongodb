373345c4778c904fc35a95634cbe4451f4a27e5b
Lilia Levchenko <liliya.benatti@gmail.com>
2024-06-05 19:00:20 +0200

    upd controllers

---
+++ src/controllers/contacts.js
@@ -0,0 +1,33 @@
+import { getAllContacts, getContactsById } from '../services/contacts';
+
+export const getAllContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    data: contacts,
+    message: 'Successfully found contacts!',
+  });
+};
+
+export const getContactsByIdController = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactsById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with id ${contactId} not found!`,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      data: contact,
+      message: `Successfully found contact with id ${contactId}!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
