50af1623da4bcb4987bcab3ac273e573dc2ac571
Lilia Levchenko <liliya.benatti@gmail.com>
2024-06-21 20:15:21 +0200

    registering user

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 import { env } from './utils/env.js';
 
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
@@ -30,7 +30,7 @@ export const setupServer = () => {
     });
   });
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use('*', notFoundHandler);
 
